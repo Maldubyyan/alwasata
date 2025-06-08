52d606593ca0975d70a0d7442d6381ff1f7d4f9b
Maldubyyan <mmk.d.uni@gmail.com>
2025-06-09 00:30:56 +0300

    Add PORT env support

--- src/application.py
+++ src/application.py
@@ -1,11 +1,19 @@
 from flask import Flask
+import os
+
 
 application = Flask(__name__)
 
+
 @application.route('/')
 def hello():
-    return '<h1>مرحباً بك في منصة الواسطة!</h1><p>تم تشغيل التطبيق بنجاح على AWS Elastic Beanstalk.</p>'
+    return (
+        '<h1>مرحباً بك في منصة الواسطة!</h1>'
+        '<p>تم تشغيل التطبيق بنجاح على AWS Elastic Beanstalk.</p>'
+    )
+
 
 # هذا هو النقطة الرئيسية للتطبيق
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT', 5000))
+    application.run(host='0.0.0.0', port=port)
